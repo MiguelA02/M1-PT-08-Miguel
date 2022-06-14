6b8ec91030b07ca8b50554106073a3732652e05f
MiguelA02 <101620100+MiguelA02@users.noreply.github.com>
2022-06-14 17:57:47 -0600

    Homework ready

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,22 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
+  let acc = 0;
+  num = num.split('').reverse();
+  for(let i = 0; i < num.length;i++){
+    acc += parseInt(num[i]) * Math.pow(2,i);
+  }
+  return acc;
 
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  let array = [];
+  while(num >= 1){
+    array.push(Math.floor(num%2));
+    num = num/2;
+  }
+  return array.reverse().join('');
 }
 
 
